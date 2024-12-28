1417fabc3f2868402031504619c87ff1eed0b85c
Thongngy <thongngy52@gmail.com>
2024-12-28 22:00:10 +0700

    Initial commit

---
+++ lib/page/homepage.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Row(
+        children: [
+           Expanded(
+          child: Container(
+            color: Colors.pink,
+          ),
+        ),
+        Expanded(
+          child: Container(
+            color: Colors.red,
+          ),
+        ),
+          Expanded(
+          child: Container(
+            color: Colors.grey,
+          ),
+        ),
+      ],
+      ),
+    );
+    
+  }
+}
